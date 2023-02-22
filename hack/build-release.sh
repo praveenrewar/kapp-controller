07970afda26afc76e1df72c7c67119a1106ad1c4
Praveen Rewar <8457124+praveenrewar@users.noreply.github.com>
2023-02-23 02:07:30 +0530

    Do not expose development values to kc package bundle
    Structure the config values to have a clear separation of values
    Update dev-deploy.sh to use these config values
    
    Signed-off-by: Praveen Rewar <8457124+praveenrewar@users.noreply.github.com>

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -8,7 +8,12 @@ mkdir -p .imgpkg/
 # makes the get_kappctrl_ver function available (scrapes version from git tag)
 source $(dirname "$0")/version-util.sh
 
-ytt -f config/ -f config-release -v kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
+export version="$(get_kappctrl_ver)"
+
+sed 's/v0.0.0/'"$version"'/' config/package-bundle/config/deployment.yml > tmp/deployment.yml
+mv tmp/deployment.yml config/package-bundle/config/deployment.yml
+
+ytt -f config/package-bundle/config -f config/release -v dev.version="$version" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
 
 shasum -a 256 ./tmp/release.yml
 
